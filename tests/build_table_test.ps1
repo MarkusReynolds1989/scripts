9ee696d066d4ffe5d5fdb8baca0ccd05bd5674c6
Markus Reynolds <markusreynolds1989@gmail.com>
2023-04-14 05:41:31 -0500

    Fix tests

--- tests/build_table_test.ps1
+++ tests/build_table_test.ps1
@@ -1,4 +1,11 @@
 $content = Get-Content "fake.csv"
+$goal = Get-Content "fake_output.md" -Raw
 $result = & "../src/Build-Table.ps1" "," "markdown" $content
 
-Write-Output $result
+if ($result -eq $goal.ToString()) {
+    Write-Output "Pass"
+}
+else {
+    Write-Output "Fail"
+    Write-Output $result
+}
